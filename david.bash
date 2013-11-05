6e182b41cd5a916144bda7d19ad8f5a85fb5fa0c
David Lu!! <davidvlu@gmail.com>
2013-11-05 09:45:07 -0600

    David.bash

---
+++ david.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+alias hosts_timeout.py='sudo env PATH=$PATH hosts_timeout.py'
+alias latex-mk='latex-mk --pdflatex'
+alias gitst='git status .'
+alias fucking_catkin='catkin_make -DCMAKE_BUILD_TYPE=Release'
+alias seas='ssh dvl1@ssh.seas.wustl.edu'
+
