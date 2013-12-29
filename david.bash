99b691795e24f7432223b6dfaeb215027c8fc0f9
David Lu!! <davidvlu@gmail.com>
2013-12-29 10:57:16 -0600

    Add shortcut to seas2

--- david.bash
+++ david.bash
@@ -6,5 +6,6 @@ alias gitst='git status .'
 alias gitdiff='git difftool -g -d'
 alias fucking_catkin='catkin_make -DCMAKE_BUILD_TYPE=Release ; if [ $? -eq 0 ]; then aplay /home/dlu/Sounds/smw_power-up.wav 2> /dev/null; else aplay /home/dlu/Sounds/smw_pipe.wav 2> /dev/null; fi'
 alias seas='ssh dvl1@ssh.seas.wustl.edu'
+alias seas2='ssh dvl1@ssh2.seas.wustl.edu'
 
 PS1="! \W/ > "
