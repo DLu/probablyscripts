d230d077e20a9e6d72ce7fafb0f155fbabb880d6
David Lu <davidvlu@gmail.com>
2014-01-05 19:15:34 -0600

    Add package lister to alias

--- david.bash
+++ david.bash
@@ -7,5 +7,5 @@ alias gitdiff='git difftool -g -d'
 alias fucking_catkin='catkin_make -DCMAKE_BUILD_TYPE=Release ; if [ $? -eq 0 ]; then aplay /home/dlu/Sounds/smw_power-up.wav 2> /dev/null; else aplay /home/dlu/Sounds/smw_pipe.wav 2> /dev/null; fi'
 alias seas='ssh dvl1@ssh.seas.wustl.edu'
 alias seas2='ssh dvl1@ssh2.seas.wustl.edu'
-
+alias list_installed='dpkg --get-selections | grep -v deinstall | awk -F" " '"'"'{ print $1 }'"'"' | grep'
 PS1="! \W/ > "
