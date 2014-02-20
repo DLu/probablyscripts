4303904931ebb9a7ea6ea7fd5aac99687d293058
David Lu!! <davidvlu@gmail.com>
2014-02-20 11:41:10 -0600

    Quick package cleanup

--- wave_editor.py
+++ wave_editor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 import sys
-from audio_clip import AudioClip
-from wave_gui import WaveGUI
+from wave_manip.audio_clip import AudioClip
+from wave_manip.wave_gui import WaveGUI
 
 if __name__=='__main__':
     clip = AudioClip(sys.argv[1])
