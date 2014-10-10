795e464255ec30090ad0c0ca4181e9b3f24fc062
David V. Lu!! <davidvlu@gmail.com>
2014-10-10 14:00:42 -0500

    Spellcheck script

---
+++ spellcheck.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import subprocess
+import os
+import mimetypes
+
+for root, dirs, files in os.walk('.'):
+    for fn in files:
+        if '~' in fn:
+            continue
+        full = root + '/' + fn
+        ftype = mimetypes.guess_type(full)[0]
+        
+        if not ftype or not ('text' in ftype or 'x-httpd-php' in ftype):
+            print ftype
+            continue
+        s = raw_input(full)
+        if 'q' in s or 'x' in s:
+            exit(1)
+        elif s == 'sf':
+            break
+        elif 's' in s:
+            continue
+        else:
+            if 'x-tex' in ftype:
+                mode = ['--mode=tex']
+            elif 'html' in ftype or 'php' in ftype:
+                mode = ['--mode=html']
+            else:
+                mode = []
+            subprocess.call(['aspell'] + mode + ['-x', '-c', full])
+
