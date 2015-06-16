d208a2042f2afe51fe3b68129f8b942dc04e8b26
David Lu <davidvlu@gmail.com>
2015-06-16 12:06:36 -0400

    Formatting filtering rss

--- filter_rss.py
+++ filter_rss.py
@@ -1,15 +1,20 @@
 from xml.dom import minidom
+
+def remove_all(s, a, b):
+    while a in s:
+        s = s.replace(a,b)
+    return s
+
 xmldoc = minidom.parse('/home/dlu/Desktop/rebelforce.xml')
 rss = xmldoc.childNodes[0]
 channel = rss.getElementsByTagName('channel')[0]
 itemlist = channel.getElementsByTagName('item')
-#print(itemlist[0].attributes['name'].value)
 for s in itemlist:
     if not 'Oxygen' in s.getElementsByTagName('title')[0].childNodes[0].data:
         channel.removeChild(s)
-#    else:
-#        print s.getElementsByTagName('title')[0].childNodes[0].data
-#    print(s.attributes['name'].value)
 
 with open('/home/dlu/Desktop/filtered.xml', 'w') as f:
-    f.write(xmldoc.toprettyxml().encode('utf8'))
+    core = xmldoc.toprettyxml().encode('utf8')
+    core = remove_all(core, '\t\n', '\n')
+    core = remove_all(core, '\n\n', '\n')
+    f.write(core)
