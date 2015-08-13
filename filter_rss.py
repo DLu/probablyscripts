11a147e3ae5185b3947bf09bb97fb2f1f5b820f0
David Lu <davidvlu@gmail.com>
2015-08-13 18:37:10 -0400

    Arguments and web access

--- filter_rss.py
+++ filter_rss.py
@@ -1,19 +1,26 @@
 from xml.dom import minidom
+import sys
+from urllib import urlopen
 
 def remove_all(s, a, b):
     while a in s:
         s = s.replace(a,b)
     return s
 
-xmldoc = minidom.parse('/home/dlu/Desktop/rebelforce.xml')
+original, output, pattern = sys.argv[1:]
+
+xmldoc = minidom.parse(urlopen(original))
 rss = xmldoc.childNodes[0]
 channel = rss.getElementsByTagName('channel')[0]
 itemlist = channel.getElementsByTagName('item')
+n = len(itemlist)
+c = 0
 for s in itemlist:
-    if not 'Oxygen' in s.getElementsByTagName('title')[0].childNodes[0].data:
+    if not pattern in s.getElementsByTagName('title')[0].childNodes[0].data:
         channel.removeChild(s)
-
-with open('/home/dlu/Desktop/filtered.xml', 'w') as f:
+        c+=1
+print "Removed %d/%d items"%(c,n)
+with open(output, 'w') as f:
     core = xmldoc.toprettyxml().encode('utf8')
     core = remove_all(core, '\t\n', '\n')
     core = remove_all(core, '\n\n', '\n')
