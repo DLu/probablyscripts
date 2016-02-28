5364d6891f0b00cbc066fffab6add11c9d2f0d3f
David Lu <davidvlu@gmail.com>
2016-02-28 18:17:13 -0500

    Refactor add_to_podcast

--- add_to_podcast.py
+++ add_to_podcast.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python
 
-from datetime import datetime
+from podcast import YamlPodcast
+
 import youtube_dl
 from youtube_dl.postprocessor.ffmpeg import FFmpegExtractAudioPP
 from youtube_dl.utils import encodeFilename
-from webhelpers.feedgenerator import DefaultFeed, Enclosure #python-webhelpers
-import subprocess
-import yaml
+
 import sys
 import os
 import urllib2
 
-FOLDER = '/home/dlu/public_html/podcast'
-HOSTNAME = 'http://gonzo.probablydavid.com/podcast'
-#FOLDER = '.'
-DFILENAME = '%s/podcast.yaml' % FOLDER
-FILENAME = '%s/podcast2.xml' % FOLDER
-
-
-def formatDate():
-    dt = datetime.now()
-    return dt.strftime("%a, %d %b %Y %H:%M:%S -0500")
-
-
-def download_file(url):
-    fmt = FOLDER + u'/%(title)s.%(ext)s'
+def download_file(url, out_folder):
+    fmt = out_folder + u'/%(title)s.%(ext)s'
     ext = "mp3"
     ydl = youtube_dl.YoutubeDL({'outtmpl': fmt})
     ydl.add_post_processor(FFmpegExtractAudioPP(preferredcodec=ext))
@@ -42,86 +29,48 @@ def to_local_name(url):
     return url.replace(HOSTNAME, FOLDER)
 
 
-def download_base_file(url):
+def download_base_file(url, out_folder='.'):
     split = urllib2.urlparse.urlsplit(url)
     base = os.path.basename(split.path)
 
     response = urllib2.urlopen(url)
     contents = response.read()
 
-    outfile = '%s/%s' % (FOLDER, base)
+    outfile = '%s/%s' % (out_folder, base)
     f = open(outfile, 'w')
     f.write(contents)
     f.close()
 
-    return outfile, ''
+    return base, ''
 
-
-data = yaml.load(open(DFILENAME, 'r'))
-
+yaml = '/home/dlu/public_html/podcast/david_misc.yaml'
+files = []
 for arg in sys.argv[1:]:
+    if arg[-4:]=='yaml':
+        yaml = arg
+    else:
+        files.append(arg)
+        
+podcast = YamlPodcast(yaml)
+
+for arg in files:
     title = ''
     if 'http' in arg:
         if '.mp3' in arg:
-            filename, title = download_base_file(arg)
+            filename, title = download_base_file(arg, podcast.folder)
             description = ''
         else:
-            filename, title, description = download_file(arg)
+            filename, title, description = download_file(arg, podcast.folder)
     else:
         filename = arg
         description = ''
-    size = os.path.getsize(filename)
+    
     if len(title) == 0:
-        title = raw_input(filename + "?")
-
-    filename = os.path.abspath(filename)
-    if FOLDER in filename:
-        filename = filename[len(FOLDER) + 1:]
-    else:
-        print "Invalid filename! %s" % filename
-        exit(0)
+        title = raw_input(filename + "? ")
 
     if len(title) <= 1:
-        base = os.path.split(filename)
-        title = os.path.splitext(base[1])[0]
-    data.append({'title': title,
-                 'filename': HOSTNAME + "/" + filename,
-                 'length': size,
-                 'type': 'audio/mpeg',
-                 'date': formatDate()})
+        title = os.path.splitext(filename)[0]
+        
+    podcast.add_episode(title, filename, description)
 
-yaml.dump(data, open(DFILENAME, 'w'))
-
-
-feed = DefaultFeed(
-    title="David's Miscallaneous Podcasts",
-    link="http://gonzo.probablydavid.com/",
-    description="[description]")
-
-for item in data:
-    e = Enclosure(item['filename'], str(item['length']), item['type'])
-    if not os.path.exists(to_local_name(item['filename'])):
-        print item['filename']
-    feed.add_item(
-        title=item['title'],
-        categories=["Podcasts"],
-        link=HOSTNAME,
-        enclosure=e,
-        description=item.get(
-            'description',
-            ''),
-        pubdate=datetime.strptime(
-            item['date'],
-            '%a, %d %b %Y %H:%M:%S -0500'))
-
-f = open(FILENAME, 'w')
-s = feed.writeString('utf-8')
-A = ['title', 'link', 'enclosure', 'description', 'pubDate']
-D = {'<item>': '\n<item>\n'}
-for a in A:
-    w = '</%s>' % a
-    D[w] = w + '\n'
-for a, b in D.iteritems():
-    s = s.replace(a, b)
-f.write(s)
-f.close()
+podcast.write_to_file()
---
+++ podcast.py
@@ -0,0 +1,73 @@
+from webhelpers.feedgenerator import DefaultFeed, Enclosure #python-webhelpers
+import os, os.path
+import yaml
+import datetime
+import stat
+
+DATE_FORMAT = '%a, %d %b %Y %H:%M:%S -0500'
+
+class Podcast(DefaultFeed):
+    def __init__(self, title, link, description, image):
+        DefaultFeed.__init__(self, title=title, link=link, description=description)
+        self.link = link
+
+    def add_episode(self, url, title, size, description='', date=None):
+        if date is None:
+            date = datetime.datetime.now()        
+            
+        e = Enclosure(url, str(size), 'audio/mpeg')
+        self.add_item(title=title, categories=['Podcasts'], link=self.link,
+                      enclosure=e, description=description, pubdate=date)
+
+    def __repr__(self):
+        s = self.writeString('utf-8')
+        A = ['title', 'link', 'enclosure', 'description', 'pubDate']
+        D = {'<item>': '\n<item>\n'}
+        for a in A:
+            w = '</%s>' % a
+            D[w] = w + '\n'
+        for a, b in D.iteritems():
+            s = s.replace(a, b)
+        return s
+
+class YamlPodcast(Podcast):
+    def __init__(self, yaml_filename):
+        self.filename = yaml_filename
+        self.data = yaml.load(open(yaml_filename, 'r'))
+        Podcast.__init__(self, self.data['title'], self.data['link'], 
+                            self.data.get('description', ''),
+                            self.data.get('image', ''))
+
+        self.basedir = os.path.dirname(self.filename)
+        self.folder = self.data.get('folder', self.basedir)
+        for ep in self.data.get('episodes', []):
+            self.add_yaml_episode(ep)
+            
+    def add_yaml_episode(self, ep):        
+        date = datetime.datetime.strptime(ep['date'],DATE_FORMAT)
+        url = self.link + '/' + ep['filename']
+        Podcast.add_episode(self, url, ep['title'], ep['length'], 
+                            ep.get('description', ''), date)
+                                
+    def add_episode(self, title, filename, description=''):
+        date = datetime.datetime.now().strftime(DATE_FORMAT)
+        full_filename = os.path.join(self.folder, filename)
+        print full_filename
+        size = os.path.getsize(full_filename)
+        st = os.stat(full_filename)
+        os.chmod(full_filename, st.st_mode | stat.S_IROTH)
+        ep = {'title': title, 'filename': filename,
+              'length': size, 'date': date}
+        self.data['episodes'].append(ep)
+        yaml.dump(self.data, open(self.filename, 'w'))
+        self.add_yaml_episode(ep) 
+        
+    def check_files(self):
+        for item in self.data.get('episodes', []):    
+            if not os.path.exists(to_local_name(item['filename'])):
+                print item['filename']
+                
+    def write_to_file(self):
+        filename = os.path.join( self.folder, self.data.get('output_fn', 'podcast.xml') )
+        with open(filename, 'w') as f:
+            f.write(str(self))
