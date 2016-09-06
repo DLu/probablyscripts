65d241c913fcc19cc58c3efccb4aee7a97ca5a3b
David Lu <davidvlu@gmail.com>
2016-09-05 22:18:44 -0400

    Some anagram work

--- anagram.py
+++ anagram.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
 
-import sys
+import argparse
 import collections
 import copy
 
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
-
 def has_letter(letter, counts, pcount):
     return counts[letter] > pcount
-
 
 def to_counts(s):
     counts = collections.defaultdict(int)
@@ -18,7 +16,6 @@ def to_counts(s):
             return None
         counts[c] += 1
     return counts
-
 
 class Node:
     processed = 0
@@ -31,7 +28,7 @@ class Node:
         self.left = None
         self.right = None
         self.state = state
-        self.N = 0
+        self.word_length = 0
 
     def split(self, queue):
         if Node.total == 0:
@@ -48,31 +45,30 @@ class Node:
             if c == 0:
                 continue
             r = c / float(len(queue))
-            # print letter, r
             if abs(r - .50) < best:
                 best = abs(r - .5)
                 bestL = letter
 
         self.letter = bestL
-        self.N = sum(self.state.values())
+        self.word_length = sum(self.state.values())
         pcount = self.state[self.letter]
         a = []
         b = []
         for word, counts in queue:
-            if len(word) == self.N:
+            if len(word) == self.word_length:
                 self.words.append(word)
             elif has_letter(self.letter, counts, pcount):
                 a.append((word, counts))
             else:
                 b.append((word, counts))
-        # print "***********" , len(a), len(b), len(self.words)
+
         Node.processed += len(self.words)
         r = Node.processed / float(Node.total)
         s = '%.2f' % r
         if s != Node.last:
             Node.last = s
             print '%d%% Complete' % (int(100 * r))
-        # print Node.processed / float(Node.total)
+        
         if len(a) > 0:
             lstate = copy.copy(self.state)
             lstate[self.letter] += 1
@@ -81,12 +77,11 @@ class Node:
         if len(b) > 0:
             self.right = Node(self.state)
             self.right.split(b)
-        # print "X"
 
     def get_anagrams(self, counts, state=collections.defaultdict(int), n=None):
         if n is None:
             n = sum(counts.values())
-        if n == self.N:
+        if n == self.word_length:
             return self.words
         elif has_letter(self.letter, counts, state[self.letter]):
             state[self.letter] += 1
@@ -95,19 +90,43 @@ class Node:
         elif self.right:
             return self.right.get_anagrams(counts, state, n)
         return []
-words = []
+        
+    def get_anagram_sets(self, counts):
+        remaining_letters = sum(counts.values())
+        if remaining_letters == 0:
+            return []
+        if len(self.words)==0:
+            if counts[self.letter]>0:
+                c2 = dict(counts)
+                c2[self.letter]-=1
+                if self.left:
+                    return self.left.get_anagram_sets(c2)
+            elif self.right:
+                return self.right.get_anagram_sets(counts)
+        else:
+            
+                
 
-for word in open(sys.argv[1], 'r').readlines():
-    word = word.strip()
-    if len(word) == 0:
-        continue
-    letters = True
-    counts = to_counts(word)
-    if counts is not None:
-        words.append((word, counts))
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('basis', nargs='+')
+    parser.add_argument('-d', '--dictionary', default='/etc/dictionaries-common/words')
+    
+    args = parser.parse_args()
+    
+    words = []
 
+    for word in open(args.dictionary, 'r').readlines():
+        word = word.strip()
+        if len(word) == 0:
+            continue
+        counts = to_counts(word)
+        if counts is not None:
+            words.append((word.lower(), counts))
+    print 'Dictionary Loaded'
 
-root = Node()
-root.split(words)
+    root = Node()
+    root.split(words)
+    print 'Dictionary Processed'
 
-print root.get_anagrams(to_counts(sys.argv[2]))
+    print root.get_anagrams(to_counts(' '.join(args.basis)))
