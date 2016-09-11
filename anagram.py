4cbef3b21f88c41b8721f015a0f1171b864d5fe0
David Lu <davidvlu@gmail.com>
2016-09-11 15:19:10 -0400

    Working multiword anagram

--- anagram.py
+++ anagram.py
@@ -6,6 +6,21 @@ import copy
 
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
+def count_cmp(c1, c2):
+    s1 = sum(c1.values())
+    s2 = sum(c2.values())
+    if s1 != s2:
+        #print count_str(c1), ';', count_str(c2), ';', s1, s2, s1<s2
+        return s1 < s2
+    for letter in sorted(set(c1.keys() + c2.keys())):
+        a = c1[letter]
+        b = c2[letter]
+        if a != b:
+            #print count_str(c1), '|', count_str(c2), '|', letter, a, b, a<b
+            return a < b
+    return None
+
+
 class LetterSet:
     def __init__(self, counts, words=None):
         self.counts = counts
@@ -14,7 +29,7 @@ class LetterSet:
         else:
             self.words = words
         self.letter_count = sum(counts.values())
-        
+
     def __lt__(self, other):
         if self.letter_count != other.letter_count:
             return self.letter_count < other.letter_count
@@ -24,7 +39,35 @@ class LetterSet:
             if a != b:
                 return a < b
         return None
-        
+
+class AnagramTree:
+    def __init__(self, parent=None, words=None):
+        if words is None:
+            self.words = []
+        else:
+            self.words = words
+        self.children = []
+        self.parent = parent
+
+    def valid_count(self, count):
+        if self.parent is None:
+            return True
+        c = to_counts(self.words[0])
+        if not count_cmp(count, c):
+            return False
+        return self.parent.valid_count(count)
+
+    def __repr__(self, depth=0):
+        s = ' ' * depth
+        if self.parent is None:
+            inc = 0
+        else:
+            s += str(self.words)
+            s += '\n'
+            inc = 1
+        for child in sorted(self.children, key=lambda d: -len(d.words[0])):
+            s += child.__repr__(depth+inc)
+        return s
 
 def has_letter(letter, counts, pcount):
     return counts[letter] > pcount
@@ -36,6 +79,9 @@ def to_counts(s):
             return None
         counts[c] += 1
     return counts
+
+def count_str(counts):
+    return ' '.join(['%s%d'%x for x in counts.items() if x[1]>0])
 
 class Node(LetterSet):
     processed = 0
@@ -86,7 +132,7 @@ class Node(LetterSet):
         if s != Node.last:
             Node.last = s
             print '%d%% Complete' % (int(100 * r))
-        
+
         if len(a) > 0:
             lstate = copy.copy(self.counts)
             lstate[self.letter] += 1
@@ -108,35 +154,68 @@ class Node(LetterSet):
         elif self.right:
             return self.right.get_anagrams(counts, state, n)
         return []
-        
+
     def get_anagram_sets(self, counts):
         remaining_letters = sum(counts.values())
         if remaining_letters == 0:
-            return []
-        if len(self.words)==0:
-            if counts[self.letter]>0:
-                c2 = copy.copy(counts)
-                c2[self.letter]-=1
-                if self.left:
-                    return self.left.get_anagram_sets(c2)
-            elif self.right:
-                return self.right.get_anagram_sets(counts)
-        #else:
-            
-                
+            if len(self.words)>0:
+                return [self.words]
+            else:
+                return []
+        anagrams = []
+        if len(self.words)>0:
+            anagrams.append(self.words)
+        if counts[self.letter]>0 and self.left:
+            c2 = copy.copy(counts)
+            c2[self.letter]-=1
+            left_a = self.left.get_anagram_sets(c2)
+            if len(left_a)>0:
+                anagrams.extend(left_a)
+        if self.right:
+            right_a = self.right.get_anagram_sets(counts)
+            if len(right_a)>0:
+                anagrams.extend(right_a)
+        return anagrams
+
+    def get_multiword_anagram(self, counts, node=None, depth=0):
+        if node is None:
+            node = AnagramTree()
+        anagrams = self.get_anagram_sets(counts)
+        for i, words in enumerate(anagrams):
+            if depth<=0:
+                print ' '*depth + '%d/%d'%(i,len(anagrams))
+            child = AnagramTree(parent=node, words=words)
+            word = words[0]
+            pc = to_counts(word)
+            c2 = copy.copy(counts)
+            for l,c in pc.iteritems():
+                c2[l] -= c
+            valid = node.valid_count(pc)
+            if not valid:
+                continue
+            res = self.get_multiword_anagram(c2, child, depth=depth+1)
+            if res:
+                node.children.append(res)
+        if sum(counts.values())>0 and len(node.children)==0:
+            return None
+        return node
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('basis', nargs='+')
     parser.add_argument('-d', '--dictionary', default='/etc/dictionaries-common/words')
-    
+    parser.add_argument('-s', '--single', action='store_true')
+    parser.add_argument('-ss', '--sub', action='store_true')
+    parser.add_argument('-m', '--multi', action='store_true')
     args = parser.parse_args()
-    
+
     words = []
 
     for word in open(args.dictionary, 'r').readlines():
         word = word.strip()
         if len(word) == 0:
+            continue
+        if len(word)==1 and word not in 'ai':
             continue
         counts = to_counts(word)
         if counts is not None:
@@ -146,5 +225,12 @@ if __name__=='__main__':
     root.split(words)
     print 'Dictionary Processed'
 
-#    print root.get_anagrams(to_counts(' '.join(args.basis)))
-    print root.get_anagram_sets(to_counts(' '.join(args.basis)))
+    counts = to_counts(' '.join(args.basis))
+    if args.single:
+        print root.get_anagrams(counts)
+    if args.sub:
+        print root.get_anagram_sets(counts)
+
+    if args.multi:
+        tree = root.get_multiword_anagram(counts)
+        print tree
