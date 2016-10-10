b72b35cb56b4a7e0b04092e60448ea805a05c9b6
David V. Lu <davidvlu@gmail.com>
2016-10-10 12:17:22 -0400

    Podcast updates

--- pocket_podcast.py
+++ pocket_podcast.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 from podcast import YamlPodcast
-from pocketpy.auth import auth
 from pocketpy.pocket import retrieve
 from pocketpy.tags import add_tags
 import urllib2
-import re, os
+import re, os, json
 import pprint
 
 
 def download_base_file(url, out_folder='/home/dlu/public_html/podcast/'):
-    split = urllib2.urlparse.urlsplit(url)
-    base = os.path.basename(split.path)
+    if 'podtrac' in url:
+        base = url[ url.rindex('/')+1 : ]
+    else:
+        split = urllib2.urlparse.urlsplit(url)
+        base = os.path.basename(split.path)
 
     response = urllib2.urlopen(url)
     contents = response.read()
@@ -26,32 +28,45 @@ class NPR:
     URL_PATT = re.compile('npr\.org')
     T_PAT = re.compile('<title>(.*)</title>')
     M_PAT = re.compile('<li class="audio-tool audio-tool-download">\s*<a href="([^"]*)"')
-
+    
+class WBUR:
+    URL_PATT = re.compile('wbur\.org')
+    T_PAT = re.compile('<title>(.*)</title>')
+    M_PAT = re.compile('<a href="([^"]*)" class="article-audio-dl" title="Download the audio"')
+    
+PATTERNS = [NPR, WBUR]
+    
 yaml = '/home/dlu/public_html/podcast/david_misc.yaml'
 
 podcast = YamlPodcast(yaml)
 
-config = auth({})
+config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.creds')
+config = json.load(open(config_path))
 
 for key, entry in retrieve(config, verbose=True).iteritems():
     if 'podcast' in entry.get('tags', {}):
         continue
-    url = entry.get('resolved_url', entry.get('given_url', None))
-    if url is None:
-        continue
-    if 'npr' in url:
-        page = urllib2.urlopen(url).read()
-        m = NPR.M_PAT.search(page)
-        if m:
+    try:
+        url = entry.get('resolved_url', entry.get('given_url', None))
+        if url is None:
+            continue
+        for pattern in PATTERNS:
+            if not pattern.URL_PATT.search(url):
+                continue
+            page = urllib2.urlopen(url).read()
+            m = pattern.M_PAT.search(page)
+            if not m:
+                continue
+            
             fn = download_base_file(m.group(1))
-
-            m2 = NPR.T_PAT.search(page)
+            m2 = pattern.T_PAT.search(page)
             if m2:
                 title = m2.group(1)
             else:
                 title = m.group(1)
             podcast.add_episode(title, fn, '')
             add_tags(config, [key], 'podcast')
-
+    except:
+        None
 
 podcast.write_to_file()
