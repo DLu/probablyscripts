22fdaeb88371a101bf17e44025aedac295ac26cb
David V. Lu <davidvlu@gmail.com>
2017-07-06 15:54:30 -0400

    Enhanced pattern rename

--- pattern_rename.py
+++ pattern_rename.py
@@ -3,15 +3,17 @@
 import argparse
 import os
 import shutil
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('needle')
 parser.add_argument('replacement', nargs='?')
+parser.add_argument('-g', '--git', action='store_true')
 
 args = parser.parse_args()
 print args
 for path, folders, files in os.walk('.'):
-    for file in files:
+    for file in files + folders:
         if args.needle not in file:
             continue
         full_path = os.path.join(path, file)
@@ -21,4 +23,7 @@ for path, folders, files in os.walk('.'):
         replacement_path = os.path.join(path, file.replace(args.needle, args.replacement))
         if os.path.exists(replacement_path):
             print "Can't move", full_path
-        shutil.move(full_path, replacement_path)
+        if args.git:
+            subprocess.call(['git', 'mv', full_path, replacement_path])
+        else:
+            shutil.move(full_path, replacement_path)
