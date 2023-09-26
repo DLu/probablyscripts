a0cb8a043960e1d3c146adceae729749ce5bd270
David V. Lu <davidvlu@gmail.com>
2023-09-25 23:07:45 -0400

    More args for audio metadata

--- audio_metadata.py
+++ audio_metadata.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import argparse
 import pathlib
+import re
 
 import eyed3
 
@@ -11,10 +12,14 @@ key_tags = ['track_num', 'title', 'album', 'artist', 'recording_date', 'genre', 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--title_case', action='store_true')
     parser.add_argument('-w', '--write', action='store_true')
     parser.add_argument('-n', '--overwrite_numbers', action='store_true')
+    parser.add_argument('-p', '--pattern', help='regular expression - groups are track and title')
     parser.add_argument('-a', '--album')
+    parser.add_argument('-y', '--year')
     parser.add_argument('-g', '--genre')
+    parser.add_argument('-r', '--artist')
     args = parser.parse_args()
 
     rows = []
@@ -23,6 +28,9 @@ if __name__ == '__main__':
     for pattern in ['*.mp3', '*.flac']:
         tracks += folder.glob(pattern)
     tracks.sort()
+
+    if args.pattern:
+        pattern = re.compile(args.pattern)
 
     for i, filename in enumerate(tracks):
         id3 = eyed3.load(filename)
@@ -33,6 +41,24 @@ if __name__ == '__main__':
             id3.tag = eyed3.id3.tag.Tag()
         if args.overwrite_numbers:
             id3.tag.track_num = i + 1, len(tracks)
+
+        if args.pattern:
+            m = pattern.match(filename.stem)
+            if m:
+                id3.tag.track_num = int(m.group(1)), len(tracks)
+                if id3.tag.title:
+                    pass
+                else:
+                    id3.tag.title = f'{m.group(2)}'
+
+            if args.title_case:
+                id3.tag.title = id3.tag.title.title()
+
+        if args.artist:
+            id3.tag.artist = args.artist
+
+        if args.year:
+            id3.tag.recording_date = args.year
 
         if args.album:
             id3.tag.album = args.album
